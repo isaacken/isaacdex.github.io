8b79c0c04450d85361b0cb3ba7ab635c54b65783
Isaac <isaackennedyfds@gmail.com>
2019-05-23 11:34:59 -0300

    Busca e paginação da API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,107 @@
 import React,{Component} from 'react';
 import PokemonItem from './PokemonItem';
+import axios from 'axios';
 import './assets/css/App.css';
+import { type } from 'os';
+
+var image_folder = process.env.PUBLIC_URL + '/assets/img';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+      pokemon: [],
+      loaded: false
+    }  
+  }
+
+  pad = (num, size) => {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
+  componentWillMount = async () => {
+    this.getData();
+  }
+
+  getData = async () => {
+    this.setState({loaded: false});
+    let limit = (this.state.page - 1) * 12;
+    let pokemonList = [];
+    let pokemon = axios.get('https://pokeapi.co/api/v2/pokemon/?offset='+limit+'&limit=12').then((response) => {
+      pokemon = response.data.results;
+      pokemon.forEach(async (e) => {
+        axios.get(e.url)
+        .then((response) => {
+          pokemonList.push({
+            name: response.data.name,
+            number: this.pad(response.data.id,3),
+            types: response.data.types
+          });
+          this.setState({loaded: this.state.loaded})
+        });
+      });
+      this.setState({pokemon: pokemonList, loaded: true});
+    });
+  }
+
+  nextPage = () => {
+    this.setState({page: ++this.state.page}); 
+    this.getData();
+  }
+
+  prevPage = () => {
+    if (this.state.page > 1) {
+      this.setState({page: --this.state.page}); 
+      this.getData();
+    }
+  }
+
   render() {
-    return (
-      <div className="pokedex">
-        <div className='pokemon-list'>
-          <PokemonItem 
-            pokemon_name='Bulbasaur'
-            pokedex_number='1'
-            type_1='grass'
-            type_2='poison' />
+    if (this.state.loaded) {
+      this.state.pokemon.sort((a,b) => a.number - b.number);
+      return (
+        <div className='pokedex'>
+          <div className='header'>
+            <h1><img src={image_folder + '/logo.svg'} /></h1>
+          </div>
+          <div className='pokemon-list'>
+            {
+              this.state.pokemon.map((e,k) => {
+                if (e.types[1]) {
+                  return (
+                    <PokemonItem 
+                      pokemon_name={e.name}
+                      pokedex_number={e.number}
+                      type_1={e.types[1].type.name}
+                      type_2={e.types[0].type.name} />
+                  );
+                } else {
+                  return (
+                    <PokemonItem 
+                      pokemon_name={e.name}
+                      pokedex_number={e.number}
+                      type_1={e.types[0].type.name} />
+                  );
+                }
+              })
+            }
+          </div>
+          <div className='pagination'>
+            <img className='nav-btn' onClick={this.prevPage} src={image_folder+'/arrow_lft.svg'}/>
+            <div className='page-range'>
+              {(this.state.page - 1) * 12 + 1} - {(this.state.page - 1) * 12 + 12}
+            </div>
+            <img className='nav-btn' onClick={this.nextPage} src={image_folder+'/arrow_rgt.svg'}/>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>Loading...</div>
+      );
+    } 
   }
 }
--- src/PokemonItem.js
+++ src/PokemonItem.js
@@ -1,21 +1,35 @@
 import React,{Component} from 'react';
 import './assets/css/App.css';
+import './assets/css/Types.css';
+import ReactTooltip from 'react-tooltip';
 
 var image_folder = process.env.PUBLIC_URL + '/assets/img';
 
 export default class PokemonItem extends Component {
+  renderType2 = () => {
+    if (this.props.type_2)
+      return (
+        <a href={'https://www.pokemon.com/br/pokedex/?type='+this.props.type_2} target='_blank'>
+          <img className='pokemon-type pokemon-type-2' data-tip={this.props.type_2} src={image_folder+'/types/'+this.props.type_2+'.svg'} />
+        </a>
+      );
+  }
+
   render() {
     return (
         <div className='pokemon-item'>
-            <div className='pokemon-item-title'>
+            <ReactTooltip />
+            <figure className='pokemon-image-container'>
+                <img src={'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'+this.props.pokedex_number+'.png'} />
+            </figure>
+            <a href={'https://www.pokemon.com/br/pokedex/'+this.props.pokedex_number} target='_blank' className={this.props.type_1 + '-title pokemon-item-title'}>
                 <h2>{this.props.pokemon_name}</h2>
                 <span className='pokemon-id'>#{this.props.pokedex_number}</span>
-            </div>
-            <figure className='pokemon-image-container'>
-                <img src={'https://pokeres.bastionbot.org/images/pokemon/'+this.props.pokedex_number+'.png'} />
-            </figure>
-            <img className='pokemon-type-1' src={image_folder+'/types/'+this.props.type_1+'.svg'} />
-            <img className='pokemon-type-2' src={image_folder+'/types/'+this.props.type_2+'.svg'} />
+            </a>
+            <a href={'https://www.pokemon.com/br/pokedex/?type='+this.props.type_1}>
+              <img className='pokemon-type pokemon-type-1' target='_blank' data-tip={this.props.type_1} src={image_folder+'/types/'+this.props.type_1+'.svg'} />
+            </a>
+            {this.renderType2()}
         </div>
     );
   }
