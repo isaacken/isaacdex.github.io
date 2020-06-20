a322a8027af238c11506a10f8de9df5a806481ab
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-06-20 03:01:13 -0300

    Updates

---
+++ precache-manifest.c7843f1c4167abdd2a164fb00b7f255a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26abc371fa911b5eea8b320243be508",
+    "url": "/pokedex_react/index.html"
+  },
+  {
+    "revision": "c2362e54b5475db0be8a",
+    "url": "/pokedex_react/static/css/main.759eb7f5.chunk.css"
+  },
+  {
+    "revision": "d15d014dcce7ee074240",
+    "url": "/pokedex_react/static/js/2.48351d4c.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/pokedex_react/static/js/2.48351d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2362e54b5475db0be8a",
+    "url": "/pokedex_react/static/js/main.c22705d0.chunk.js"
+  },
+  {
+    "revision": "b51dcaf70f1f8a2a2528",
+    "url": "/pokedex_react/static/js/runtime-main.7a8ba860.js"
+  }
+]);
