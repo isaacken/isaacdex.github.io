c2b8b9bbf34d8077d7248b58f7d7cc1350698446
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-06-20 02:45:32 -0300

    Including build

---
+++ build/precache-manifest.eb858b16403844ef731c1026485c62a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1576caaf5e7510a76713e765418fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3407cd74af226990c09",
+    "url": "/static/css/main.759eb7f5.chunk.css"
+  },
+  {
+    "revision": "d15d014dcce7ee074240",
+    "url": "/static/js/2.48351d4c.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.48351d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3407cd74af226990c09",
+    "url": "/static/js/main.25cb32c5.chunk.js"
+  },
+  {
+    "revision": "4793a726737bbec1e6de",
+    "url": "/static/js/runtime-main.4d6fe92c.js"
+  }
+]);
